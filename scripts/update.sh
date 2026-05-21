924fd173a39707ff7db70c33461f2eacebc2e77c
zhennann <zhen.nann@icloud.com>
2026-05-21 09:53:21 +0800

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
 pnpm --dir './zova-ui-empty' update
-pnpm --dir './zova-ui-empty' prerun
 pnpm --dir './zova-ui-quasar' update
-pnpm --dir './zova-ui-quasar' prerun
 pnpm --dir './zova-ui-vuetify' update
+pnpm --dir './zova-ui-empty' prerun
+pnpm --dir './zova-ui-quasar' prerun
 pnpm --dir './zova-ui-vuetify' prerun
